4acc5e86ca2841a781b01e1943b8c511ed687081
Arseni Dylko <dylko.ak@phystech.edu>
2024-04-06 00:13:29 +0300

    added unit tests for king class, refactored code of moveTo() method.

--- src/piece/Pawn.java
+++ src/piece/Pawn.java
@@ -1,5 +1,9 @@
-import chess.*;
+package piece;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
+import chess.*;
 public class Pawn extends Piece {
 
 	private short dir;
@@ -18,47 +22,63 @@ public class Pawn extends Piece {
 		return colour.charAt(0)+"P";
 	}
 
-	/* A pawn can move only to a direction 'dir' fixed by its color, 
-	 * it can go to immediate next column (in that direction), and 
+	/* A pawn can move only to a direction 'dir' fixed by its color,
+	 * it can go to immediate next column (in that direction), and
 	 * move straight or one step diagonally to kill a piece.
-	 * 
+	 *
 	 * It can also move 2 steps in an initial move.
 	 * */
-	public boolean canMoveTo(Cell dest, Board board)
-	{	
-		//System.out.println(dest.col+" "+dest.row);
-		//Case 1: Normal move or killing move.
-		if(  (dest.row == (char)(currentPos.row + this.dir)) && 
-			((dest.col == currentPos.col && dest.getPiece()==null)
-			|| ((dest.col == currentPos.col -1 || dest.col == currentPos.col +1)
-			 && dest.getPiece()!=null && dest.getPiece().getColour()!=this.colour))
-		  )
-		{	
-			return true;
+
+	protected ArrayList<Cell> getAllMoves(Board board)
+	{
+		this.moves = new ArrayList<Cell>();
+		//Case 1: Normal move
+		if(board.colourAt((char)(currentPos.row + this.dir),
+			currentPos.col)	== null)
+		{
+			moves.add(board.getCellAt((char)(currentPos.row + this.dir),
+				currentPos.col));
 		}
-		//Case 2: initial move.
+
+		//Case 2: killing move.
+		if(board.colourAt((char)(currentPos.row + this.dir),
+			(char)(currentPos.col-1))	!= this.colour)
+		{
+			moves.add(board.getCellAt((char)(currentPos.row + this.dir),
+				(char)(currentPos.col-1)));
+		}
+		if(board.colourAt((char)(currentPos.row + this.dir),
+			(char)(currentPos.col+1))	!= this.colour)
+		{
+			moves.add(board.getCellAt((char)(currentPos.row + this.dir),
+				(char)(currentPos.col+1)));
+		}
+
+		//Case 3: initial move.
 		//Both, destination and middle cell should be empty.
-		else if(dest.col == currentPos.col && currentPos == orig 
-				&& dest.row == (char)(orig.row + 2*this.dir)
-				&& dest.getPiece() == null 
-				&& board.colourAt((char)(orig.row + this.dir), orig.col)==null)
-		{	
-			return true;
+		if( currentPos.equals(orig) &&
+			board.colourAt((char)(currentPos.row + 2*this.dir),
+				currentPos.col)	== null &&
+			board.colourAt((char)(currentPos.row + this.dir),
+				currentPos.col)	== null)
+		{
+			moves.add(board.getCellAt((char)(currentPos.row + 2*this.dir),
+				currentPos.col));
 		}
-		else
-			return false;
+
+		return this.moves;
 	}
-	
-	/* A pawn can get upgraded to a higher piece (Queen or Knight 
+
+	/* A pawn can get upgraded to a higher piece (Queen or Knight
 	 * or Rook or Bishop) if it reaches the terminal cell of any column.
 	 * */
 	@Override
 	protected boolean moveTo(Cell dest, Board board)
 	{
-		if(dest.row == orig.row+6)
+		if(dest.row == Board.rowMax)
 		{
 			currentPos.setPiece(null);
-			
+
 			System.out.println("You can update your piece.");
 			System.out.println("Enter Q for Queen, R for rook, K for knight and B for bishop.");
 			Scanner sc = new Scanner(System.in);
@@ -70,19 +90,19 @@ public class Pawn extends Piece {
 			sc.close();
 
 			if(newPiece == 'Q')
-			{	
+			{
 				dest.setPiece(new Queen(this.colour, dest));
 			}
 			else if(newPiece == 'K')
-			{	
+			{
 				dest.setPiece(new Knight(this.colour, dest));
 			}
 			else if(newPiece == 'R')
-			{	
+			{
 				dest.setPiece(new Rook(this.colour, dest));
 			}
 			else if(newPiece == 'B')
-			{	
+			{
 				dest.setPiece(new Bishop(this.colour, dest));
 			}
 			currentPos = null;
--- src/piece/Queen.java
+++ src/piece/Queen.java
@@ -1,4 +1,7 @@
 package piece;
+
+import java.util.ArrayList;
+
 import chess.Board;
 import chess.Cell;
 
@@ -16,177 +19,32 @@ public class Queen extends Piece {
 	/* A queen can move only where a rook or a bishop can move.
 	 * i.e. it can move in same row or column (like a rook)
 	 * or it can move in same diagonal (like a bishop)
-	 * I.e. it can go to dest if sum(row, column) or diff(row, column) of 
+	 * I.e. it can go to dest if sum(row, column) or diff(row, column) of
 	 * its current cell and dest are same.
 	 * */
-	public boolean canMoveTo(Cell dest, Board board)
+	protected ArrayList<Cell> getAllMoves(Board board)
 	{
-		final char dr =dest.row, dc= dest.col;
-		final char cr =currentPos.row, cc= currentPos.col;
-		if(	( ((dr + dc) == (cr + cc)) || (dr-dc)== (cr-cc) ) 
-			&& (dest.getPiece() == null || dest.getPiece().getColour() != this.colour)
-		  )
-		{	
-			//If the bishop is to be moved along a constant sum diagonal.
-			if((dc+dr) == (cr+cc))
-			{	final int sum = dr+dc;
-				if(dest.row > currentPos.row)
-				{	for(int i=currentPos.row+1; 
-						i<=Board.rowMax && i<=(sum-Board.colMin); i++)
-					{	if(dest.row == i)
-							return true;
-						//sum-i will be column character/number.
-						if(board.colourAt((char)i, (char)(sum-i)) == this.colour)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of its own color, it can't move ahead.
+		this.moves = new ArrayList<Cell>();
 
-						if(board.colourAt((char)i, (char)(sum-i)) != null)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of opposite color, it can't move ahead.
-					}
-				}
-				else
-				{
-					for(int i=currentPos.row-1; 
-							i>=Board.rowMin && i>=(sum-Board.colMax); i--)
-					{	if(dest.row == i)
-							return true;
-						//sum-i will be column character/number.
-						if(board.colourAt((char)i, (char)(sum-i)) == this.colour)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of its own color, it can't move ahead.
+		//If the queen is to be moved along a constant sum diagonal.
+		//final int sum = currentPos.row + currentPos.col;
+		moves.addAll(this.movesInDir(board, 1, -1));
+		moves.addAll(this.movesInDir(board, -1, 1));
 
-						if(board.colourAt((char)i, (char)(sum-i)) != null)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of opposite color, it can't move ahead.
-					}
-				}
-			}
-			//If the bishop is to be moved along a constant difference diagonal.
-			else
-			{	final int diff = dr-dc;
-				if(dest.row > currentPos.row)
-				{	for(int i=currentPos.row+1; 
-						i<=Board.rowMax && i<=(diff-Board.colMax); i++)
-					{	if(dest.row == i)
-							return true;
-						//i-(dr-dc) will be column character/number.
-						if(board.colourAt((char)i, (char)(i-diff)) == this.colour)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of its own color, it can't move ahead.
+		//If the queen is to be moved along a constant difference diagonal.
+		//final int diff = currentPos.row - currentPos.col;
+		moves.addAll(this.movesInDir(board, 1, 1));
+		moves.addAll(this.movesInDir(board, -1, -1));
 
-						if(board.colourAt((char)i, (char)(i-diff)) != null)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of opposite color, it can't move ahead.
-					}
-				}
-				else
-				{
-					for(int i=currentPos.row-1; 
-							i>=Board.rowMin && i>=(diff-Board.colMin); i--)
-					{	if(dest.row == i)
-							return true;
-						//i-(dr-dc) will be column character/number.
-						if(board.colourAt((char)i, (char)(i-diff)) == this.colour)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of its own color, it can't move ahead.
+		//If the queen is to be moved along a row.
+		moves.addAll(this.movesInDir(board, 1, 0));
+		moves.addAll(this.movesInDir(board, -1, 0));
 
-						if(board.colourAt((char)i, (char)(i-diff)) != null)
-						{	return false;
-						}
-						//if bishop is blocked by a piece of opposite color, it can't move ahead.
-					}
-				}
-			}
-			assert(false);
-			return false;
-		}
-		//move like a rook.
-		else if((dest.row == currentPos.row || dest.col == currentPos.col) 
-			&& (dest.getPiece()==null || dest.getPiece().getColour() != this.colour))
-		{	
-			//If the desired cell is in the same column, check whether
-			//there is any intruding piece of the same color as this rook.
-			if(dest.col == currentPos.col)
-			{	if(dest.row > currentPos.row)
-				{	for(int i=currentPos.row+1; i<=Board.rowMax; i++)
-					{	if(dest.row == i)
-							return true;
-						if(board.colourAt((char)i, currentPos.col) == this.colour)
-						{	return false;
-						}
-						//if rook is blocked by a piece of its own color, it can't move ahead.
+		//If the queen is to be moved along a column.
+		moves.addAll(this.movesInDir(board, 0, 1));
+		moves.addAll(this.movesInDir(board, 0, -1));
 
-						if(board.colourAt((char)i, currentPos.col) != null)
-						{	return false;
-						}
-						//if rook is blocked by a piece of opposite color, it can't move ahead.
-					}
-				}
-				else
-				{	for(int i=currentPos.row-1; i>=Board.rowMin; i--)
-					{	if(dest.row == i)
-							return true;
-						if(board.colourAt((char)i, currentPos.col) == this.colour)
-						{	return false;
-						}
-						//if rook is blocked by a piece of its own color, it can't move ahead.
-
-						if(board.colourAt((char)i, currentPos.col) != null)
-						{	return false;
-						}
-						//if rook is blocked by a piece of opposite color, it can't move ahead.
-					}
-				}
-			}
-
-			//If the desired cell is in the same row, check whether
-			//there is any intruding piece of the same color as this rook.
-			//or any piece of opposite color.
-			if(dest.row == currentPos.row)
-			{	if(dest.col > currentPos.col)
-				{	for(int i=currentPos.col+1; i<=Board.colMax; i++)
-					{	if(dest.col == i)
-							return true;
-						if(board.colourAt(currentPos.row, (char)i) == this.colour)
-						{	return false;
-						}
-						//if rook is blocked by a piece of its own color, it can't move ahead.
-
-						if(board.colourAt(currentPos.row, (char)i) != null)
-						{	return false;
-						}
-						//if rook is blocked by a piece of opposite color, it can't move ahead.
-					}
-				}
-				else
-				{	for(int i=currentPos.col-1; i>=Board.colMin; i--)
-					{	if(dest.col == i)
-							return true;
-						if(board.colourAt(currentPos.row, (char)i) == this.colour)
-						{	return false;
-						}
-
-						if(board.colourAt(currentPos.row, (char)i) != null)
-						{	return false;
-						}
-					}
-					//if rook is blocked by a piece of its own color, it can't move ahead.
-				}	
-			}
-
-			assert(false);
-			return false;
-		}
-		else
-			return false;
+		return this.moves;
 	}
-	
+
 }
